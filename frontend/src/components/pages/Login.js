ea3abbbdda8d5cc0a297580a887340c00ff5f1f0
MFlanders <mflanders06@gmail.com>
2021-11-20 15:57:08 -0700

    inputs gathering data

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -3,43 +3,54 @@ import { Link } from "react-router-dom";
 
 function Login() {
 
-    const [loginEmail, setLoginEmail] = useState('');
+    const [loginEmail, setLoginEmail]       = useState('');
     const [loginPassword, setLoginPassword] = useState('');
-    const [regEmail, setRegEmail] = useState('');
-    const [regPassword1, setRegPassword1] = useState('');
-    const [regPassword2, setRegPassword2] = useState('');
+    const [regEmail, setRegEmail]           = useState('');
+    const [regPassword1, setRegPassword1]   = useState('');
+    const [regPassword2, setRegPassword2]   = useState('');
 
-    function onChangeLoginEmail(val)    { setLoginEmail( prevEmail => val) }
-    function onChangeLoginPassword(val) { setLoginPassword( prevPass => val) }
-    function onChangeRegEmail(val)      { setRegEmail( prevEmail => val) }
+    function onChangeLoginEmail(val)     { setLoginEmail( prevEmail => val) }
+    function onChangeLoginPassword(val)  { setLoginPassword( prevPass => val) }
+    function onChangeRegEmail(val)       { setRegEmail( prevEmail => val) }
     function onChangeRegPass1(val)       { setRegPassword1( prevPass => val ) }
     function onChangeRegPass2(val)       { setRegPassword2( prevPass => val ) }
+
+    function onClickLogin(){
+        console.log(loginEmail);
+        console.log(loginPassword);
+    }
+
+    function onClickReg(){
+        console.log(regEmail);
+        console.log(regPassword1);
+        console.log(regPassword2);
+    }
 
     return(
         <>
             <div className="placeholder">
                 <div className="loginForm">
                     <h2>Login</h2>
-                    <form>
-                        <label for="loginEmail">email:</label>
-                        <input type="text" id="loginEmail" name="loginEmail"></input>
+                    <label>email:</label>
+                    <input type="text" id="loginEmail"onChange={e => onChangeLoginEmail(e.target.value)} ></input>
 
-                        <label for="loginPassword">password:</label>
-                        <input type="password" id="loginPassword" name="loginPassword"></input>
-                    </form>
+                    <label>password:</label>
+                    <input type="password" id="loginPassword" onChange={e => onChangeLoginPassword(e.target.value)} ></input>
+
+                    <input type="button" id="submitLogin" name="submitLogin" value="Login" onClick={ e => onClickLogin()}></input>
                 </div>
                 <div className="regForm">
                     <h2>Register</h2>
-                    <form>
-                        <label for="regEmail">email:</label>
-                        <input type="text" id="regEmail" name="regEmail"></input>
+                    <label>email:</label>
+                    <input type="text" id="regEmail" onChange={e => onChangeRegEmail(e.target.value)} ></input>
 
-                        <label for="regPassword1">password:</label>
-                        <input type="password" id="regPassword1" name="regPassword1"></input>
+                    <label>password:</label>
+                    <input type="password" id="regPassword1" onChange={e => onChangeRegPass1(e.target.value)} ></input>
 
-                        <label for="regPassword2">confirm password:</label>
-                        <input type="password" id="regPassword2" name="regPassword2"></input>
-                    </form>
+                    <label>confirm password:</label>
+                    <input type="password" id="regPassword2" onChange={e => onChangeRegPass2(e.target.value)} ></input>
+
+                    <input type="button" id="submitRegister" value="Register" onClick={ e => onClickReg()}></input>
                 </div>
                 <Link to="/">Back to Welcome</Link>
             </div>
