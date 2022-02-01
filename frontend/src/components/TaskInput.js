8fb6a19df9bf9ffcb83f8e8bb7d512700c9dd76b
MFlanders <mflanders06@gmail.com>
2022-01-31 20:53:38 -0700

    Framework for inputs

--- frontend/src/components/TaskInput.js
+++ frontend/src/components/TaskInput.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 
 function TaskInput() {
+
+    function createTask(){
+        console.log("hi");
+        return;
+    }
+
     return(
         <>
             <div>This is the display for task inputs</div>
 
+            <div>Title: </div>
+            <input type="text"></input>
+
+            <div>Task: </div>
+            <input type="text"></input>
+
+            <div>Due Date: </div>
+            <input type="date"></input>
+
+            <input type="button" value="Create" onClick={createTask} ></input>
         </>
     )
 }
