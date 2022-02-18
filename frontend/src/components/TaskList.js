e30cf457eb3b4bcd16b784d6380f8c514f3a63d3
MFlanders <mflanders06@gmail.com>
2022-02-17 22:08:00 -0700

    mapped takslist into display (rough)

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function TaskList() {
+
+    const [theTaskList, setTheTaskList] = useState();
+
+    let getTaskList = async() => {
+        await axios
+            .get('/api/tasks/tasks')
+            .then(tasks => {
+                setTheTaskList(tasks.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    console.log(theTaskList);
+
+    useEffect(() => {
+        getTaskList();
+    },[]);
+
     return(
         <>
+        {theTaskList.map((value, index) => (
+            <div key={value.task_key}>
+                {value.task_name}{value.task_description}{value.created_date}{value.due_date}
+            </div>
+        ))}
             <div>This is the display for the task list</div>
 
         </>
