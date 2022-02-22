d6876b28f62fca94caa761911b0ffd1574c9d4a1
MFlanders <mflanders06@gmail.com>
2022-02-22 06:11:36 -0700

    error catching - retrieve data before display

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -16,23 +16,28 @@ function TaskList() {
             })
     }
 
-    console.log(theTaskList);
+    //console.log(theTaskList);
 
     useEffect(() => {
         getTaskList();
     },[]);
 
-    return(
-        <>
-        {theTaskList.map((value, index) => (
-            <div key={value.task_key}>
-                {value.task_name}{value.task_description}{value.created_date}{value.due_date}
-            </div>
-        ))}
-            <div>This is the display for the task list</div>
+    if (!theTaskList){
+        return(<>Loading</>)
+    }
+    else{
+        return(
+            <>
+                {theTaskList.map((value, index) => (
+                    <div key={value.task_key}>
+                        {value.task_name}{value.task_description}{value.created_date}{value.due_date}
+                    </div>
+                ))}
+                <div>This is the display for the task list</div>
 
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default TaskList;
